5714ae447d5861425938f551889746f8b0194325
Leek - Ubuntu Server <contact@maliktillman.com>
2019-11-05 17:30:18 -0500

    Minor fixes; Made dev-origin

--- scripts/custom.js
+++ scripts/custom.js
@@ -13,6 +13,26 @@ var bottomHeader = document.getElementById("header-bottom-container");
 /* Null top style */
 var topNull = bottomHeader.style.top;
 
+/* DOM Query for Catogory/Filter Label, List Container, and Items */
+var filterLabel = document.getElementsByClassName("ProductList-filter-dropdownToggle-label");
+var filterList = document.getElementsByClassName("ProductList-filter-list");
+var filterItems = document.getElementsByClassName("ProductList-filter-list-item");
+
+/* Toggler for Catogory/Filter | 0 is closed and 1 is open */
+var filterToggler = 0;
+
+/* Catagory/Filter List Container Height */
+var filterHeight = 0;
+
+/* Calculate Catagory/Filter List Container Height */
+for(item in filterItems) {
+  filterHeight += 20;
+}
+
+window.addEventListener("load", function(){
+  console.log("Loaded");
+}, false)
+
 /* Event Listener for Deteting Scroll */
 window.addEventListener("scroll", function() {
   /* Catch the Current Scroll Position */
@@ -33,4 +53,21 @@ window.addEventListener("scroll", function() {
 
   /* Mobile/Negative Scroll Fix */
   lastScrollPos = thisScrollPos <= 0 ? 0 : thisScrollPos;
+}, false);
+
+/* Event Listener for Deteting Mobile Catogory/Filter Label Toggles */
+filterLabel[0].addEventListener("click", function () {
+  filterToggler ^= true;
+
+  console.log(filterHeight);
+
+  if(filterToggler) {
+    // Open
+    filterList[0].style.height = filterHeight + "px";
+    filterList[0].style.opacity = 1;
+  } else {
+    // Close
+    filterList[0].style.height = "0px";
+    filterList[0].style.opacity = 0;
+  }
 }, false);
